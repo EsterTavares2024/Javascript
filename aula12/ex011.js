6cd6c89f43d8bd8bd0a9eaaa8632507db66ea082
Ester Tavares <138616986+EsterTavares2024@users.noreply.github.com>
2023-08-03 20:54:19 -0300

    Aula 12 ex 11 ao 13

---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 64.5
+console.log (`Você tem ${idade} anos.`)
+if (idade <16) {
+console.log('Não vota')
+} else if (idade >= 16 && idade <18 || idade >65) {
+        console.log('Voto opcional') 
+} else {
+    console.log('Voto é Obrigatório')
+}
+    
---
+++ aula12/ex012.js
@@ -0,0 +1,10 @@
+
+var hora = 18.5
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora < 12) {
+    console.log('Bom dia')
+} else if (hora <=18) {
+    console.log('Boa tarde!')
+} else {
+    console.log('Boa noite!')
+}
